199d251fcbb1424cb13fe4ce9576e9518b20d652
RubiaHermans <147212420+RubiaHermans@users.noreply.github.com>
2024-03-17 02:35:59 -0700

    css,html and jquery drop down

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+console.log("hello there");
+
+$("#viewproject").click(function(){
+   console.log("hello peixe");
+  });
+
+$("#flip").click(function(){
+ console.log("hello boi");
+    $("#panel").slideDown("slow");
+  });
+
