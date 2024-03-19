29f0190434733e586cce811c958949800ebcb78a
RubiaHermans <147212420+RubiaHermans@users.noreply.github.com>
2024-03-19 16:40:35 -0700

    changes about final details

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-KV05DF5B0Z');
 
 
 console.log("hello there");
@@ -14,6 +10,6 @@ $("#viewproject").click(function(){
 
 $("#flip").click(function(){
  console.log("hello boi");
-    $("#panel").slideDown("slow");
+    $("#panel").slideToggle("slow");
   });
 
