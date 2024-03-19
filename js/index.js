c437a3467c0139090d20219181bbfb494fd0eb5f
RubiaHermans <147212420+RubiaHermans@users.noreply.github.com>
2024-03-18 21:12:55 -0700

    website changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KV05DF5B0Z');
+
+
 console.log("hello there");
 
 $("#viewproject").click(function(){
